970b77b0cb47d4fcdaad1e46c04516e0bd5ce882
Matt Nicks <mnicks@ford.com>
2025-10-21 09:35:38 +0100

    feat(US1295086): enable cors support

--- api/src/main/java/com/interview/financeApi/FinanceApiApplication.java
+++ api/src/main/java/com/interview/financeApi/FinanceApiApplication.java
@@ -2,14 +2,11 @@ package com.interview.financeApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class FinanceApiApplication {
-
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		SpringApplication.run(FinanceApiApplication.class, args);
 	}
 }
