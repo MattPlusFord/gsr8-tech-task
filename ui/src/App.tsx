bd9b82d9d46a907bb1592cbc13db438691bc834b
Matt Nicks <mnicks@ford.com>
2025-11-12 10:03:01 +0000

    feat(US3456735): apply error boundary to app

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,7 +1,15 @@
+import './components/layout/layout.css';
+import './App.css';
+import ErrorBoundary from "./components/errors/ErrorBoundary.tsx";
 
 export const App = () => {
+    const errorSupportUrl = import.meta.env.VITE_ERROR_SUPPORT_DOMAIN;
+
     return (
         <>
+            <ErrorBoundary supportUrl={errorSupportUrl}>
+                <></>
+            </ErrorBoundary>
         </>
     )
 }
